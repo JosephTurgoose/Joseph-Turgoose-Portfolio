8ca5d0c25c918400e6930e5757528d0e8d227d52
Joseph Turgoose <21jturgoose@gmail.com>
2026-03-09 17:28:10 -0600

    deploy 3

--- Joseph-Turgoose-Portfolio/vite.config.js
+++ Joseph-Turgoose-Portfolio/vite.config.js
@@ -1,7 +1,8 @@
+// https://vite.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Joseph-Turgoose-Portfolio/',
 })
